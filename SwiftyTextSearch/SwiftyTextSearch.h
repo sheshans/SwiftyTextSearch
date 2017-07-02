83081ad7277d6253e00b6de930c4e1c211d55fbc
Sheshans Yadav <sheshans.r.yadav@gmail.com>
2017-07-02 11:53:26 +0530

    Initial Commit

---
+++ SwiftyTextSearch/SwiftyTextSearch.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyTextSearch.h
+//  SwiftyTextSearch
+//
+//  Created by apple on 7/2/17.
+//  Copyright © 2017 Mirraw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyTextSearch.
+FOUNDATION_EXPORT double SwiftyTextSearchVersionNumber;
+
+//! Project version string for SwiftyTextSearch.
+FOUNDATION_EXPORT const unsigned char SwiftyTextSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTextSearch/PublicHeader.h>
+
+
